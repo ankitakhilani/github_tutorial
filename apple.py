9026f630c9d1d23c1ff5b5f7e01365c615b21082
BlazinglySlow <44846809+BlazinglySlow@users.noreply.github.com>
2020-03-11 21:21:18 -0700

    Update apple.py
    changed value of babies from 100 to 200

--- apple.py
+++ apple.py
@@ -1,5 +1,5 @@
 # Simple Pythong lines: Fibonacci, tuple assignment
 parents, babies = (1, 1)
-while babies < 100:
+while babies < 200:
     print ('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
