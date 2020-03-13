16d52386bde156b25e53dc029478112cf1ec9798
ankitakhilani <61543154+ankitakhilani@users.noreply.github.com>
2020-03-13 10:52:58 +0530

    Update apple.py
    changed babies to 450

--- apple.py
+++ apple.py
@@ -1,5 +1,5 @@
 # Simple Pythong lines: Fibonacci, tuple assignment
 parents, babies = (1, 1)
-while babies < 300:
+while babies < 450:
     print ('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
